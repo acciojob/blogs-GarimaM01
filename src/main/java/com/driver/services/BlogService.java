63186995bb623b96d294b0bc01a6a6cfc57728a2
GarimaM01 <garima.mahto@sakec.ac.in>
2023-05-27 14:20:41 +0530

    1st commit

--- src/main/java/com/driver/services/BlogService.java
+++ src/main/java/com/driver/services/BlogService.java
@@ -21,13 +21,28 @@ public class BlogService {
     @Autowired
     UserRepository userRepository1;
 
-    public Blog createAndReturnBlog(Integer userId, String title, String content) {
-        //create a blog at the current time
+    @Autowired
+    ImageRepository imageRepository;
 
+    public Blog createAndReturnBlog(Integer userId, String title, String content) {
+        // Create a blog at the current time
+        Blog blog = new Blog();
+        User user = userRepository1.findById(userId).get();
+        if (user != null) {
+            blog.setUser(user);
+            blog.setTitle(title);
+            blog.setContent(content);
+            blog.setPubDate(new Date());
+            List<Blog> blogsWritten = user.getBlogList();
+            blogsWritten.add(blog);
+            user.setBlogList(blogsWritten);
+            userRepository1.save(user);
+        }
+        return blog;
     }
 
-    public void deleteBlog(int blogId){
-        //delete blog and corresponding images
-
+    public void deleteBlog(int blogId) {
+        // Delete blog and corresponding images
+        blogRepository1.deleteById(blogId);
     }
 }
--- src/main/java/com/driver/services/ImageService.java
+++ src/main/java/com/driver/services/ImageService.java
@@ -12,20 +12,45 @@ public class ImageService {
 
     @Autowired
     BlogRepository blogRepository2;
+
     @Autowired
     ImageRepository imageRepository2;
 
-    public Image addImage(Integer blogId, String description, String dimensions){
-        //add an image to the blog
-
+    public Image addImage(Integer blogId, String description, String dimensions) {
+        // Add an image to the blog
+        Blog blog = blogRepository2.findById(blogId).get();
+        Image image = new Image(blog, description, dimensions);
+        blog.getImageList().add(image);
+        blogRepository2.save(blog);
+        return image;
+        // Here I am not explicitly adding the image in the image repository because of the cascading effect
     }
 
-    public void deleteImage(Integer id){
-
+    public void deleteImage(Integer id) {
+        // Delete an image by its id
+        imageRepository2.deleteById(id);
     }
 
     public int countImagesInScreen(Integer id, String screenDimensions) {
-        //Find the number of images of given dimensions that can fit in a screen having `screenDimensions`
+        // Find the number of images of given dimensions that can fit in a screen having `screenDimensions`
+        String[] scrArray = screenDimensions.split("X"); // A = Length   X    B = Breadth
+        Image image = imageRepository2.findById(id).get();
 
+        String imageDimensions = image.getDimensions();
+        String[] imgArray = imageDimensions.split("X");
+
+        int scrl = Integer.parseInt(scrArray[0]); // A --> integer
+        int scrb = Integer.parseInt(scrArray[1]); // B --> integer
+
+        int imgl = Integer.parseInt(imgArray[0]); // A --> integer
+        int imgb = Integer.parseInt(imgArray[1]); // B --> integer
+
+        return calculateNoOfImages(scrl, scrb, imgl, imgb);
+    }
+
+    private int calculateNoOfImages(int scrl, int scrb, int imgl, int imgb) {
+        int lenC = scrl / imgl; //
+        int lenB = scrb / imgb;
+        return lenC * lenB;
     }
 }
